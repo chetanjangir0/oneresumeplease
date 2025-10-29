3d501728f60ecd9c12b3d27e4c9d72f32ed3dab7
Chetan Jangir <chetanrjj2002@gmail.com>
2025-10-29 19:37:01 +0530

    add basic json encoding/decoding and write headers and initialize go.mod

--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+type User struct {
+	FirstName string `json:"first_name"`
+	Id        int  `json:"id"`
+}
+
+var users = []User{}
+
+func (a *app) getUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// encode users slice to json
+	err := json.NewEncoder(w).Encode(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+
+}
+func (a *app) createUserHandler(w http.ResponseWriter, r *http.Request) {
+
+	// decode request body
+	var payload User
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	u := User{
+		FirstName: payload.FirstName,
+		Id:        payload.Id,
+	}
+	users = append(users, u)
+	w.WriteHeader(http.StatusCreated)
+}
 
 type app struct {
 	addr string
 }
 
-func (a *app) getUserHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("user list..."))
-}
-
-func (a *app) postUserHandler(w http.ResponseWriter, r *http.Request){
+func (a *app) postUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("created user"))
 }
 
@@ -22,7 +55,7 @@ func main() {
 	// initialze the servermux
 	mux := http.NewServeMux()
 	srv := &http.Server{
-		Addr: app.addr,
+		Addr:    app.addr,
 		Handler: mux,
 	}
 
