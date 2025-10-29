e6ad27da6bdedb809ef0034eabd80421f726d6a9
Chetan Jangir <chetanrjj2002@gmail.com>
2025-10-29 18:58:04 +0530

    add serverMux for simper routing

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
-type server struct {
+type app struct {
 	addr string
 }
 
-func (s *server) ServeHTTP(http.ResponseWriter, *http.Request) {
-	panic("unimplemented")
+func (a *app) getUserHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("user list..."))
+}
+
+func (a *app) postUserHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("created user"))
 }
 
 func main() {
-	s := &server{addr: ":8080"}
-	http.ListenAndServe(s.addr, s)
+	app := &app{addr: ":8080"}
+
+	// initialze the servermux
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr: app.addr,
+		Handler: mux,
+	}
+
+	mux.HandleFunc("GET /users", app.getUserHandler)
+	mux.HandleFunc("POST /users", app.postUserHandler)
+	srv.ListenAndServe()
 }
